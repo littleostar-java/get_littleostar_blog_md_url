e3a459cec2352ad8d9af2ac85a2dab5f6f072e7b
littleOStar <qqduxingzhe@163.com>
2018-12-10 10:47:26 +0800

    rename json file; make json inline line; update tools.java method name;

--- src/main/java/tools/Tools.java
+++ src/main/java/tools/Tools.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Tools {
-    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static Gson gson = new GsonBuilder().create();
 
     public static void do_write_json_to_file(LinkedList<String> list, HashMap<String, LinkedList<MdBean>> hashMap) {
         String json_array = gson.toJson(list);
@@ -22,21 +22,21 @@ public class Tools {
 
 //        System.out.println(json_array);
 //        System.out.println(json_map);
-        write_text_to_file("md-array.json", json_array);
-        write_text_to_file("md-map.json", json_map);
+        write_text_to_file("md_array.json", json_array);
+        write_text_to_file("md_map.json", json_map);
     }
 
-    private static void write_text_to_file(String fileName, String text) {
+    private static void write_text_to_file(String write_to_fileName, String text) {
         try {
             String path_parent = System.getProperty("user.dir") + File.separator + "json_";
             File file_parent = new File(path_parent);
             if (!file_parent.exists()) {
-                file_parent.mkdir();
+                boolean mkdir = file_parent.mkdir();
             }
-//            String dir = System.getProperty("user.dir") + File.separator + "json_" + File.separator + fileName;
-            File file = new File(file_parent, fileName);
+//            String dir = System.getProperty("user.dir") + File.separator + "json_" + File.separator + write_to_fileName;
+            File file = new File(file_parent, write_to_fileName);
             if (!file.exists()) {
-                file.createNewFile();
+                boolean newFile = file.createNewFile();
             }
             FileWriter fileWriter = new FileWriter(file);
             PrintWriter printWriter = new PrintWriter(fileWriter);
@@ -46,7 +46,7 @@ public class Tools {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            System.out.println(fileName + "write success ... ");
+            System.out.println(write_to_fileName + "write success ... ");
         }
     }
 
